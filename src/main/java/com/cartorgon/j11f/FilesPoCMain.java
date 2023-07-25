4b48a1d3fca28ff692a76e5151dd7a41c6b07eb6
Carlos Torres Gonzalez <e_ctor01@mercadona.es>
2023-07-25 09:52:38 +0200

    Reading from file

--- src/main/java/com/cartorgon/j11f/FilesPoCMain.java
+++ src/main/java/com/cartorgon/j11f/FilesPoCMain.java
@@ -6,12 +6,27 @@ import java.nio.file.StandardOpenOption;
 
 public class FilesPoCMain {	
 	public static final void main(final String[] args) {		
-		try {
-			System.out.println("Writing string in file...");			
-			Files.writeString(Paths.get("C:/", "tmp", "outData.txt"), "This is another\n", StandardOpenOption.APPEND);
-			System.out.println("Done !!");
-		} catch (final Exception excp) {
-			excp.printStackTrace();
-		}		
+			writeToFile();
+			readFromFile();
 	}
+	
+	private static final void writeToFile() {
+	  try {
+      System.out.println("Writing string in file...");      
+      Files.writeString(Paths.get("C:/", "tmp", "outData.txt"), "This is another\n", StandardOpenOption.APPEND);
+      System.out.println("Done !!");
+    } catch (final Exception excp) {
+      excp.printStackTrace();
+    }
+	}
+	
+	private static final void readFromFile() {
+    try {
+      System.out.println("Reading string from file...");
+      System.out.println(Files.readString(Paths.get("C:/", "tmp", "outData.txt")));
+      System.out.println("Done !!");
+    } catch (final Exception excp) {
+      excp.printStackTrace();
+    }
+  }
 }
